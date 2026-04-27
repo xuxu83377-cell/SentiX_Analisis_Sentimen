ef35c98cff4aee25bc0d7f3f657a14862d05e99b
xuxu <xuxu83377@gmail.com>
2026-04-27 20:52:32 +0800

    add cek-npx debug endpoint

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path, include
-urlpatterns = [
-    path('', include('klasifikasi.urls')),
-]
-from django.urls import path, include
+from django.http import JsonResponse
+import shutil
+import os
+
+def cek_npx(request):
+    os.environ["PATH"] = "/usr/local/bin:/usr/bin:/bin:" + os.environ.get("PATH", "")
+    npx = shutil.which("npx")
+    node = shutil.which("node")
+    return JsonResponse({
+        "npx": npx,
+        "node": node,
+        "PATH": os.environ.get("PATH", ""),
+    })
 
 urlpatterns = [
     path('', include('klasifikasi.urls')),
+    path('cek-npx/', cek_npx),
 ]
