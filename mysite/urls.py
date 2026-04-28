d6ebb7c5d3e0d6b8a92d25e46b5a2b42f6f4fff1
xuxu <xuxu83377@gmail.com>
2026-04-28 11:23:14 +0800

    update debug endpoint cek node version

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,15 +2,22 @@ from django.urls import path, include
 from django.http import JsonResponse
 import shutil
 import os
+import subprocess
 
 def cek_npx(request):
     os.environ["PATH"] = "/usr/local/bin:/usr/bin:/bin:" + os.environ.get("PATH", "")
-    npx = shutil.which("npx")
-    node = shutil.which("node")
+    
+    node_ver = subprocess.run(["node", "--version"], capture_output=True, text=True)
+    npm_ver = subprocess.run(["npm", "--version"], capture_output=True, text=True)
+    th_exists = os.path.exists("/usr/local/bin/tweet-harvest")
+    th_ver = subprocess.run(["tweet-harvest", "--version"], capture_output=True, text=True)
+    
     return JsonResponse({
-        "npx": npx,
-        "node": node,
-        "PATH": os.environ.get("PATH", ""),
+        "node_version": node_ver.stdout.strip(),
+        "npm_version": npm_ver.stdout.strip(),
+        "tweet_harvest_exists": th_exists,
+        "tweet_harvest_version": th_ver.stdout.strip(),
+        "tweet_harvest_error": th_ver.stderr.strip()[:300],
     })
 
 urlpatterns = [
