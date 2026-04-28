52f5b9aeb666f358ccfc80c912896e1ce922b9ae
xuxu <xuxu83377@gmail.com>
2026-04-28 11:33:20 +0800

    update cek-npx

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path, include
 from django.http import JsonResponse
-import shutil
 import os
 import subprocess
 
 def cek_npx(request):
     os.environ["PATH"] = "/usr/local/bin:/usr/bin:/bin:" + os.environ.get("PATH", "")
-    
     node_ver = subprocess.run(["node", "--version"], capture_output=True, text=True)
     npm_ver = subprocess.run(["npm", "--version"], capture_output=True, text=True)
     th_exists = os.path.exists("/usr/local/bin/tweet-harvest")
     th_ver = subprocess.run(["tweet-harvest", "--version"], capture_output=True, text=True)
-    
     return JsonResponse({
         "node_version": node_ver.stdout.strip(),
         "npm_version": npm_ver.stdout.strip(),
